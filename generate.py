9310b38f3c6e1b55d2b0589fd7c8d6e0ab749395
chrisgrounds <chrisbacon2009@hotmail.com>
2021-07-31 20:42:48 +0200

    format generate

--- generate.py
+++ generate.py
@@ -7,7 +7,6 @@ def generate(ticker, tax_rate, num_years, growth_rate, std_dev, gross_margin_avg
   current_revenue = stock_info.get_income_statement(ticker).loc["totalRevenue"][0] / 1000000 # refactor
   dcf = DCF(current_revenue, std_dev, tax_rate, num_years, gross_margin_avg, operating_margin_avg, num_shares, growth_rate, discount_rate, peg)
 
-
   monteCarlo = MonteCarlo(simulations, dcf)
 
   return monteCarlo.simulate()
